59a7aea48d63ea06535e0a1939b4afd6ec3feb50
chy <1274653465@qq.com>
2024-06-11 23:12:28 +0800

    Changed dockerfile

--- Docker/launch.sh
+++ Docker/launch.sh
@@ -1,4 +1,4 @@
 docker run -it --rm=false --name sim2real_drone_racing --network host \
 	-e DISPLAY=$DISPLAY \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
-    sim2real_drone_racing:latest
+    registry.cn-hangzhou.aliyuncs.com/chy-sci/deep_drone_racing:latest
